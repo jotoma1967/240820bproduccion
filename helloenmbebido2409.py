964de50ffee73a5975386ccd37b24ab0b07b15a1
jotoma1967 <84995332+jotoma1967@users.noreply.github.com>
2024-09-15 08:50:05 -0500

    Update helloenmbebido2409.py

--- helloenmbebido2409.py
+++ helloenmbebido2409.py
@@ -56,5 +56,8 @@ def index():
     return "<h1>Hello!</h1>"
 
 if __name__ == "__main__":
+    app.run(debug=True)
+    '''
     from waitress import serve
     serve(app, host="0.0.0.0", port=10000)
+    '''
